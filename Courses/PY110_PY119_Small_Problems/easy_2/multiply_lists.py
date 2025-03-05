7d5edba7b8d2957c6b994d1f0c14a1093e0183b0
Luiz Melo <luizmelo001@gmail.com>
2025-03-05 17:22:40 -0300

    Lauch School exercises

--- Courses/PY110_PY119_Small_Problems/easy_2/multiply_lists.py
+++ Courses/PY110_PY119_Small_Problems/easy_2/multiply_lists.py
@@ -8,12 +8,22 @@ Example
 Data Structure
 
 Algorithm
-
+    -iterate through the first list with value and index
+    -use the index to acess list 2
+    -multiply value on list one by the item on list 2 with the same index
+    -return the list
 """
 
 def multiply_list(lst1, lst2):
-    pass
+    #result = []
 
+    #for idx, item in enumerate(list1):
+    #    product = item * list2[idx]
+    #    result.append(product)
+
+    multiplied = [item1 * item2 for (item1, item2) in zip(list1, list2)]
+
+    return multiplied
 
 list1 = [3, 5, 7]
 list2 = [9, 10, 11]
